d0df68e694ea3fe5e53f75039e0a49de60627ab6
Hemant Kumar <hkuma@nets.eu>
2020-11-19 19:35:46 +0100

    R01: Initial changes to create the project structure

---
+++ PromotionEngine/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            PromotionEngine();
+
+            Console.ReadLine();
+        }
+
+        private static void PromotionEngine()
+        {
+            //TODO: Add Promotion Engine logic.
+        }
+    }
+}
